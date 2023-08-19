25da89002f5bdad8471884e7602c118a59e15cc5
Balep Martin <84333489+Level237@users.noreply.github.com>
2023-08-19 09:44:22 +0100

    os module

---
+++ tutorial/3-modules.js
@@ -0,0 +1,10 @@
+// Common,every file is module (by default)
+// Modules -Encapsuled Code (only share minimum)
+const names=require('./4-names')
+const sayHi=require('./5-utils')
+const data=require('./6-alternative-flavor') 
+require('./7-mind-grenade')
+
+// sayHi('susan')
+// sayHi(names.john)
+// sayHi(names.peter)
