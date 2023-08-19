656ca44661b01409e3285f6c109cea23c313750d
Balep Martin <84333489+Level237@users.noreply.github.com>
2023-08-19 09:15:15 +0100

    other alternative export module

--- tutorial/app.js
+++ tutorial/app.js
@@ -2,8 +2,9 @@
 // Modules -Encapsuled Code (only share minimum)
 const names=require('./4-names')
 const sayHi=require('./5-utils')
+const data=require('./6-alternative-flavor')
 
-
+console.log(data)
 
 sayHi('susan')
 sayHi(names.john)
