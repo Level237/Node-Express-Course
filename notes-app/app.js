08837dae9577a3945f1292dfc832c87cf754af39
level237 <Bramslevel129@gmail.com>
2023-08-23 10:03:50 +0100

    chalk module in action

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,4 @@
-const validator=require('validator');
-const getNotes=require('./notes.js');
 
-const notes=getNotes();
-console.log(notes);
-console.log(validator.isEmail("Bramslevel@gmail.com"));
+const chalk=require('chalk');
+
+console.log(chalk.green.bold.italic.inverse("Success!"));
