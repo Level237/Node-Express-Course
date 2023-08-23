176644e0aa29e211eafddc5c28c30d7c9b43c2fb
level237 <Bramslevel129@gmail.com>
2023-08-23 15:42:04 +0100

    yargs in action

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,13 +1,50 @@
 
 const chalk=require('chalk');
+const { truncateSync } = require('fs');
+const yargs=require('yargs');
 
-console.log(chalk.green.bold.italic("Success!"));
+yargs.version("1.1.0")
 
-
-command=process.argv[2];
-
-if(command == "add"){
-    console.log("adding notes");
-}else if(command == "remove"){
-console.log("Removing Notes");
+//Create a new note
+yargs.command({
+    command:"add",
+    builder:{
+title:{
+    describe:"Note Title",
+    demandOption:true,
+    type:'string'
 }
+    },
+    description:"add a new note",
+    handler:function(argv){
+        console.log("adding a new note!",argv);
+    }
+})
+
+//create remove note
+
+yargs.command({
+    command:"remove",
+    description:"add a new note",
+    handler:function(){
+        console.log("removing the note!");
+    }
+})
+
+yargs.command({
+    command:"list",
+    description:"list note",
+    handler:function(){
+        console.log("reading the note");
+    }
+})
+
+yargs.command({
+    command:"read",
+    description:"reading note",
+    handler:function(){
+        console.log("reading the note");
+    }
+})
+//add,remove,read,list
+yargs.parse();
