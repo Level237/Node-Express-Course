d5a07ba81be0cc59bbe887015c14232c73c30cc5
level237 <Bramslevel129@gmail.com>
2023-08-23 10:40:23 +0100

    process argv:init

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,3 +2,12 @@
 const chalk=require('chalk');
 
 console.log(chalk.green.bold.italic("Success!"));
+
+
+command=process.argv[2];
+
+if(command == "add"){
+    console.log("adding notes");
+}else if(command == "remove"){
+console.log("Removing Notes");
+}
