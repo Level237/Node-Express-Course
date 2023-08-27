ec3a546de044dfbf42dd0b7d6c331b0b1e3e5116
level237 <Bramslevel129@gmail.com>
2023-08-27 19:33:43 +0100

    Callback Abstraction Challenge

--- Weather-app/app.js
+++ Weather-app/app.js
@@ -1,34 +1,18 @@
 const axios=require('axios');
 const geocode=require('./utils/geocode')
+const forecast=require('./utils/forecast')
 
-
-//const url="http://api.weatherapi.com/v1/forecast.json"
-
-//axios.get(url,{
-   // params: {
-     //   q: encodeURIComponent(address)
-     // },headers:{
-      //  key:"2495f968871c4b1db28123805232508"
-      //}
-//}).then((response)=>{
-    
-    //console.log(response.data);
-//}).catch(err=>{
-   // console.log(err);
-//})
-
-//const geoCodeUrl='http://us1.locationiq.com//v1/search?key=pk.5662601f43f70d5ab4a2af452f4348bd&q=Los Angeles&format=json'const geoCodeUrl='http://us1.locationiq.com//v1/search?key=pk.5662601f43f70d5ab4a2af452f4348bd&q=Los Angeles&format=json'
-
-//axios.get(geoCodeUrl).then((response)=>{
-    
-    //console.log(response.data[0]);
-//}).catch(err=>{
-    //console.log(err);
-//})
 
 
 
 geocode("Boston",(err,data)=>{
 console.log(data);
 })
+
+
+
+forecast("New York", (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+  })
 
