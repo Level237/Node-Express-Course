62b7cdd26cb37897adfd4b5cd08e2190eea23e3e
level237 <Bramslevel129@gmail.com>
2023-08-28 14:21:01 +0100

    Serving up Static Assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,17 +1,17 @@
 const express=require('express')
-
+const path=require('path')
 const app=express()
 
+const publicDirectoryPath=path.join(__dirname,'../public')
+
+app.use(express.static(publicDirectoryPath))
 app.get('',(req,res)=>{
 
     res.send('<h1>Weather</h1>')
 })
 
 app.get('/help',(req,res)=>{
-    res.send({
-        name:"andrew",
-        age:2
-    });
+    res.send();
 })
 
 app.get('/about',(req,res)=>{
