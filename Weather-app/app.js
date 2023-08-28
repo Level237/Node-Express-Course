a618f83d9a579ce96da30c0804df519c819c23e2
level237 <Bramslevel129@gmail.com>
2023-08-28 09:06:16 +0100

    Callback Chaining

--- Weather-app/app.js
+++ Weather-app/app.js
@@ -2,17 +2,28 @@ const axios=require('axios');
 const geocode=require('./utils/geocode')
 const forecast=require('./utils/forecast')
 
+const address=process.argv[2];
+
+if(!address){
+console.log("Please provide an address");
+}else{
+    geocode(address,(err,data)=>{
+        if(err){
+            console.log(err);
+        }
+    
+    forecast(data.location, (error, forecastData) => {
+        if(error){
+            console.log(err);
+        }
+        console.log(data.location)
+        console.log(forecastData)
+      })
+    })
+}
 
 
 
-geocode("Boston",(err,data)=>{
-console.log(data);
-})
 
 
-
-forecast("New York", (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
 
