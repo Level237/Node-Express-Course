84b528a4515f8f92a7643397a086b388dda12883
level237 <Bramslevel129@gmail.com>
2023-08-29 12:16:46 +0100

    Browser HTTP Requests with Fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,20 @@
 console.log('client side javascript file is loaded');
+
+fetch('https://puzzle.mead.io/puzzle').then((response)=>{
+    response.json().then((data)=>{
+        console.log(data);
+    })
+})
+
+fetch(`http://us1.locationiq.com//v1/search?key=pk.5662601f43f70d5ab4a2af452f4348bd&q=!&format=json`).then((response)=>{
+    response.json().then((data)=>{
+        if(data.err){
+            console.log("unable");
+        }
+        console.log(data[0]);
+    }).catch((err)=>{
+        console.log(err);
+    })
+}).catch((error)=>{
+    console.log(error);
+})
