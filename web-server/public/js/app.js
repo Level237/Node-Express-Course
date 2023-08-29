6f056c75ead703c1a660f45cf8105052a5576302
level237 <Bramslevel129@gmail.com>
2023-08-29 15:09:41 +0100

    Wiring up the User Interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -4,18 +4,25 @@ console.log('client side javascript file is loaded');
 
 const weather=document.querySelector('form')
 const search=document.querySelector('input')
+
+
 weather.addEventListener('submit',(e)=>{
+    const messageOne=document.querySelector('#message-1')
+const messageTwo=document.querySelector('#message-2')
+messageOne.textContent="loading..."
+messageTwo.textContent=''
     e.preventDefault()
     const location=search.value;
 
     fetch(`http://localhost:3000/weather?address=${location}`).then((response)=>{
     response.json().then((data)=>{
         
+        
+        messageOne.textContent=data.location
+        messageTwo.textContent=data.forecast
         console.log(data);
-        console.log(data.location);
-        console.log(data.forecast);
     }).catch((err)=>{
-        console.log(err);
+        messageOne.textContent=err;
     })
 })
 })
