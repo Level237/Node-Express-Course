79d9104d38c45a6c0178ba8ad45c45c84c90ab2e
level237 <Bramslevel129@gmail.com>
2023-08-31 13:02:41 +0100

    Setting up Mongoose

--- task-manager/src/mongoose.js
+++ task-manager/src/mongoose.js
@@ -5,22 +5,42 @@ mongoose.connect('mongodb://localhost:27017/task-manager-api').then(()=>{
     console.log("DB connection successfull!")
 })
 
-const User=mongoose.model('User',{
-    name:{
+//const User=mongoose.model('User',{
+    //name:{
+        //type:String
+    //},
+    //age:{
+        //type:Number
+    //}
+//});
+
+//const me=new User({
+    //name:"Martin",
+    //age:27
+//})
+
+//me.save().then(()=>{
+    //console.log(me);
+//}).catch((err)=>{
+   // console.log("Error!",err);
+//})
+
+const Task=mongoose.model('Task',{
+    description:{
         type:String
     },
-    age:{
-        type:Number
+    completed:{
+        type:Boolean
     }
-});
-
-const me=new User({
-    name:"Martin",
-    age:27
 })
 
-me.save().then(()=>{
-    console.log(me);
+const task=new Task({
+    description:"Best Seller of the moment",
+    completed:false
+})
+
+task.save().then(()=>{
+    console.log(task);
 }).catch((err)=>{
-    console.log("Error!",err);
+    console.log("Error",err);
 })
