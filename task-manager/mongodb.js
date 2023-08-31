94cd0607c4c9d42420eb731154bb8d527f8b9166
level237 <Bramslevel129@gmail.com>
2023-08-31 06:20:55 +0100

    Inserting Documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -9,9 +9,33 @@ client.connect()
         console.log('Connected Successfully!')
         const db=client.db(databaseName)
 
-        db.collection("users").insertOne({
-            name:"martin lunel",
-            age:25
-        })
+        //db.collection("users").insertOne({
+            //name:"martin lunel",
+            //age:25
+        //}).then((result)=>{
+            //console.log(result);
+        //}).catch((err)=>{
+            //console.log("unable insert user");
+       // })
+
+       //db.collection('users').insertMany([{
+        //name:"level",
+        //age:25
+       //},{
+        //name:"michel",
+        //age:12
+       //}])
+
+       db.collection('tasks').insertMany([{
+        description:"Welcome my children how are you",
+        completed:true
+       },{
+        description:"many insert",
+        completed:false
+       }]).then((result)=>{
+        console.log(result.acknowledged);
+       })
+
+
     })
     .catch(error => console.log('Failed to connect!', error))
