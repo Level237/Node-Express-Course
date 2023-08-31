00df1d08f5e0d45665f33e788a6d77875e5ed4d5
level237 <Bramslevel129@gmail.com>
2023-08-31 16:56:16 +0100

    Data Validation and Sanitization

--- task-manager/src/mongoose.js
+++ task-manager/src/mongoose.js
@@ -1,46 +1,69 @@
 const mongoose=require('mongoose');
+const validator=require('validator')
 
 mongoose.connect('mongodb://localhost:27017/task-manager-api').then(()=>{
     
     console.log("DB connection successfull!")
 })
 
-//const User=mongoose.model('User',{
-    //name:{
-        //type:String
-    //},
-    //age:{
-        //type:Number
-    //}
-//});
+const User=mongoose.model('User',{
+    name:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        trim:true,
+        lowercase:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email is invalid') 
+            }
+        }
+    },
+    age:{
+        type:Number,
+        default:0,
+        validate(value){
+            if(value < 0){
+                throw new Error("Age must be a positive number")
+            }
+        }
+    }
+});
 
-//const me=new User({
-    //name:"Martin",
-    //age:27
-//})
+const me=new User({
+   name:"Balep martin",
+  email:"BRAMSLEVEL@GMAIL.COM"
+})
 
-//me.save().then(()=>{
-    //console.log(me);
-//}).catch((err)=>{
-   // console.log("Error!",err);
-//})
+me.save().then(()=>{
+    console.log(me);
+}).catch((err)=>{
+    console.log("Error!",err.message);
+})
 
 const Task=mongoose.model('Task',{
     description:{
-        type:String
+        type:String,
+        require:true
     },
     completed:{
         type:Boolean
     }
 })
 
+
+
 const task=new Task({
     description:"Best Seller of the moment",
     completed:false
 })
 
-task.save().then(()=>{
-    console.log(task);
-}).catch((err)=>{
-    console.log("Error",err);
-})
+//task.save().then(()=>{
+    //console.log(task);
+//}).catch((err)=>{
+    //console.log("Error",err);
+//})
