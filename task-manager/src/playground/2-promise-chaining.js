2a0bf686fa3afa86be3c70c4a16ed5c2fc785b73
level237 <Bramslevel129@gmail.com>
2023-09-01 14:50:42 +0100

    AsyncAwait

--- task-manager/src/playground/2-promise-chaining.js
+++ task-manager/src/playground/2-promise-chaining.js
@@ -1,12 +1,25 @@
 require('../db/mongoose')
 const Task=require('../models/Task')
 
-Task.findByIdAndDelete("64f1a543e043b4d8c8d6117a")
-.then((task)=>{
-    console.log(task);
+//Task.findByIdAndDelete("64f1a543e043b4d8c8d6117a")
+//.then((task)=>{
+ //   console.log(task);
 
-    return Task.countDocuments({completed:false})
-}).then((result)=>{
+    //return Task.countDocuments({completed:false})
+//}).then((result)=>{
+    //console.log(result);
+//}).catch((err)=>{
+    //console.log(err);
+//})
+
+const deleteTaskAndCount=async (id)=>{
+    const task=await Task.findByIdAndDelete(id);
+    const count=await Task.countDocuments({completed:false});
+
+    return count;
+}
+
+deleteTaskAndCount("64f1e9bce23cb0b740127d7b").then((result)=>{
     console.log(result);
 }).catch((err)=>{
     console.log(err);
--- task-manager/src/playground/promises-chaining.js
+++ task-manager/src/playground/promises-chaining.js
@@ -3,14 +3,27 @@ const User=require('../models/User')
 
 // 64f0c94d9d0f684ff9661ff
 
-    User.findByIdAndUpdate('64f1a0d53e64fc3bcc69691b',{
-        age:3
-    }).then((user)=>{
-        console.log(user);
-        return User.countDocuments({age:3})
-    }).then((result)=>{
-        console.log(result);
-    }).catch((err)=>{
-        console.log(err);
-    })
+    //User.findByIdAndUpdate('64f1a0d53e64fc3bcc69691b',{
+        //age:3
+    //}).then((user)=>{
+        //console.log(user);
+        //return User.countDocuments({age:3})
+    //}).then((result)=>{
+        //console.log(result);
+    //}).catch((err)=>{
+        //console.log(err);
+    //})
+
+const updateAgeAndCount=async (id,age)=>{
+    const user=await User.findByIdAndUpdate(id,{age})
+    const count=await User.countDocuments({age})
+
+    return count;
+}
+
+updateAgeAndCount("64f0c94d9d0f684ff9661ff8",19).then((count)=>{
+    console.log(count);
+}).catch((err)=>{
+console.log(err);
+})
 
