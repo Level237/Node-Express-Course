031bfaa88956b39704b60e91649e09e341d31849
level237 <Bramslevel129@gmail.com>
2023-09-01 09:49:14 +0100

    Resource Creation Endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,6 +1,7 @@
 const express=require('express')
 require('./db/mongoose')
 const User=require('./models/User')
+const Task=require('./models/Task')
 
 const app=express()
 const port=process.env.PORT || 3000
@@ -10,13 +11,24 @@ app.use(express.json())
 app.post('/users',(req,res)=>{
     const user=new User(req.body)
     user.save().then(()=>{
-        res.send(user)
+        res.status(201).send(user)
     }).catch((err)=>{
         res.status(400).send({
             message:err.message
         })
     })
    
+})
+
+app.post('/tasks',(req,res)=>{
+
+    const task=new Task(req.body);
+
+    task.save().then(()=>{
+        res.status(201).send(task)
+    }).catch((err)=>{
+        res.status(400).send({error:err.message})
+    })
 })
 app.listen(port,()=>{
     console.log('Server is up in port '+port);
