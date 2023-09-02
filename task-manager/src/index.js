eb3c82c3409529a39442e168ac75fc5f4462d7e6
level237 <Bramslevel129@gmail.com>
2023-09-02 15:56:27 +0100

    Integrating AsyncAwait

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -8,72 +8,118 @@ const port=process.env.PORT || 3000
 
 app.use(express.json())
 
-app.get("/users",(req,res)=>{
-    User.find({}).then((users)=>{
-        res.status(200).send(users)
-    }).catch((e)=>{
+app.get("/users",async(req,res)=>{
+
+    try{
+        const user=await User.find({})
+        res.send(user)
+    }catch(e){
         res.status(500).send(e)
-    })
+    }
 })
 
-app.get("/users/:id",(req,res)=>{
+app.get("/users/:id",async(req,res)=>{
     const __id=req.params.id;
 
-    User.findById(__id).then((user)=>{
+    try{
+        const user=await User.findById(__id);
+
         if(!user){
             res.status(404).send()
         }
-        
         res.status(200).send(user)
-    }).catch((e)=>{
+    }catch(e){
         res.status(500).send()
-    })
+    }
+   
+    //User.findById(__id).then((user)=>{
+        //if(!user){
+            //res.status(404).send()
+        //}
+        
+        //res.status(200).send(user)
+    //}).catch((e)=>{
+        //res.status(500).send()
+    //})
    
 })
 
-app.get("/tasks",(req,res)=>{
+app.get("/tasks",async(req,res)=>{
 
-    Task.find({}).then((task)=>{
-        res.status(200).send(task)
-    }).catch((e)=>{
-        res.status(500).send(e)
-    })
+    try{
+        const tasks=await Task.find({});
+        
+        res.status(200).send(tasks);
+    }catch(e){
+
+    }
+    //Task.find({}).then((task)=>{
+        //res.status(200).send(task)
+    //}).catch((e)=>{
+        //res.status(500).send(e)
+    //})
 })
-app.post('/users',(req,res)=>{
+app.post('/users',async (req,res)=>{
     const user=new User(req.body)
-    user.save().then(()=>{
+
+    try{
+        await user.save()
         res.status(201).send(user)
-    }).catch((err)=>{
-        res.status(400).send({
-            message:err.message
-        })
-    })
+    }catch(e){
+        res.status(400).send(e)
+    }
+   
+    //user.save().then(()=>{
+        //res.status(201).send(user)
+    //}).catch((err)=>{
+        //res.status(400).send({
+            //message:err.message
+        //})
+    //})
    
 })
 
-app.post('/tasks',(req,res)=>{
+app.post('/tasks',async(req,res)=>{
 
-    const task=new Task(req.body);
+    try{
+        const task=new Task(req.body);
+        await task.save()
 
-    task.save().then(()=>{
         res.status(201).send(task)
-    }).catch((err)=>{
-        res.status(400).send({error:err.message})
-    })
+    }catch(e){
+        res.status(400).send({error:e.message})
+    }
+
+    //task.save().then(()=>{
+        //res.status(201).send(task)
+    //}).catch((err)=>{
+        //res.status(400).send({error:err.message})
+    //})
 })
 
-app.get('/task/:id',(req,res)=>{
+app.get('/task/:id',async(req,res)=>{
     const __id=req.params.id;
 
-    Task.findById(__id).then((task)=>{
+    try{
+        const task=await Task.findById(__id)
+
         if(!task){
             res.status(404).send()
         }
-        
         res.status(200).send(task)
-    }).catch((e)=>{
+    }catch(e){
         res.status(500).send()
-    })
+    }
+
+    //Task.findById(__id).then((task)=>{
+        //if(!task){
+            //res.status(404).send()
+        //}
+        
+        //res.status(200).send(task)
+    //}).catch((e)=>{
+        //res.status(500).send()
+    //})
 })
 app.listen(port,()=>{
     console.log('Server is up in port '+port);
