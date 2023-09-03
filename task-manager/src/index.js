75da923816f61b43a18f0a6effa87b1670467617
level237 <Bramslevel129@gmail.com>
2023-09-03 01:43:08 +0100

    Resource Deleting Endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -163,6 +163,32 @@ app.patch("/task/:id",async(req,res)=>{
         res.status(400).send(e)
     }
 })
+
+app.delete("/users/:id",async(req,res)=>{
+    try {
+        const user=await User.findByIdAndDelete(req.params.id)
+        if(!user){
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (error) {
+        res.status(400).send()
+    }
+})
+
+app.delete("/task/:id",async(req,res)=>{
+    try {
+        const task=await Task.findByIdAndDelete(req.params.id)
+        if(!task){
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (error) {
+        res.status(400).send()
+    }
+})
 app.listen(port,()=>{
     console.log('Server is up in port '+port);
 })
