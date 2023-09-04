48d282df12ffa4ab59221020b71614fd9cc4307e
level237 <Bramslevel129@gmail.com>
2023-09-04 10:28:28 +0100

    Express Middleware.

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -8,8 +8,18 @@ const taskRouter=require('./routers/Task')
 const app=express()
 const port=process.env.PORT || 3000
 
-app.use(express.json())
+/*app.use((req,res,next)=>{
+    if(req.method==="GET"){
+        res.send('GET Request are disable')
+    }else{
+        next()
+    }
+})*/
 
+app.use((req,res,next)=>{
+    res.status(503).send({message:"Maintenance mode enable,comming soon"})
+})
+app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
