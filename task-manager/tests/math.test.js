9d02ebb706fa47e12eff0ac961b647d6335278c1
level237 <Bramslevel129@gmail.com>
2023-09-08 15:44:21 +0100

    . Writing Your Own Tests

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,8 +1,18 @@
-const {calculateTip}=require('../src/math')
+const {calculateTip,fahrenheitToCelsius,celsiusToFahrenheit}=require('../src/math')
 
 test('Should calculate total with tip',()=>{
 
     const total=calculateTip(10,.3)
 
    expect(total).toBe(13)
+})
+
+test('Should convert 32 F to 0 C',()=>{
+    const convertCelsius=fahrenheitToCelsius(32)
+    expect(convertCelsius).toBe(0)
+})
+
+test('Should convert 32 F to 0 C',()=>{
+    const convertFahrenheit=celsiusToFahrenheit(0)
+    expect(convertFahrenheit).toBe(32)
 })
