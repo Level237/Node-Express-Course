cb4aee5c4ecc0c92b5a4ac2a370a2cba6544cb10
level237 <Bramslevel129@gmail.com>
2023-09-09 15:20:38 +0100

    Wrapping up User Tests

--- task-manager/tests/User.test.js
+++ task-manager/tests/User.test.js
@@ -82,3 +82,40 @@ test('Should not delete for unauthenticated user',async()=>{
     .send()
     .expect(401)
 })
+
+test('Should upload avatar image',async()=>{
+    await request(app)
+    .post("/users/me/avatar")
+    .set("Authorization",`Bearer ${userOne.tokens[0].token}`)
+    .attach("avatar","tests/fixtures/logo.png")
+    .expect(200)
+
+    const user=await User.findById(userOneId)
+
+    expect(user.avatar).toEqual(expect.any(Buffer))
+    
+})
+
+test('Should update valid user fields',async()=>{
+   
+   await request(app)
+    .patch("/users/me")
+    .set("Authorization",`Bearer ${userOne.tokens[0].token}`)
+    .send({
+        name:"louis"
+    })
+    .expect(200)
+
+    const user=await User.findById(userOneId)
+    expect(user.name).toEqual("louis")
+})
+
+test("Should not update invalid user field",async()=>{
+    await request(app)
+    .patch("/users/me")
+    .set("Authorization",`Bearer ${userOne.tokens[0].token}`)
+    .send({
+        location:"douala"
+    })
+    .expect(400)
+})
