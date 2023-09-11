2991bab56913c1883f799c64c366aa0e8bd59759
level237 <Bramslevel129@gmail.com>
2023-09-11 11:22:48 +0100

    Getting Started with Socket.io

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,15 +1,20 @@
 const express=require('express')
+const http=require('http')
 const path=require('path')
+const socketio=require('socket.io')
+
 const app=express()
+const server=http.createServer(app)
+const io=socketio(server)
+const publicDirectoryPath=path.join(__dirname,'../public')
 
-    const publicDirectoryPath=path.join(__dirname,'../views')
+app.use(express.static(publicDirectoryPath))
 
-    app.use(express.static(publicDirectoryPath))
-
-app.get('/',(req,res)=>{
-    res.render('index')
+io.on('connection',()=>{
+    console.log("New WebSocket connection");
 })
+
 const port=process.env.port || 3000
-app.listen(3000,()=>{
+server.listen(3000,()=>{
     console.log("Server is up in port "+port);
 })
