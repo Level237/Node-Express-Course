0e4859fbba79af3e589fc18b109b1a8af58075cf
level237 <Bramslevel129@gmail.com>
2023-09-13 10:35:11 +0100

    Form and Button States

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,13 +1,24 @@
 const socket=io()
 
+// elements
+
+const $messageForm=document.getElementById('send')
+const $messageFormInput=$messageForm.querySelector('input')
+const $messageFormButton=$messageForm.querySelector("button")
+const locationButton=document.getElementById("send-location")
 socket.on("message",(message)=>{
     console.log(message);
 })
-document.getElementById('send').addEventListener("submit",(e)=>{
+$messageForm.addEventListener("submit",(e)=>{
     e.preventDefault()
+
+    $messageFormButton.setAttribute("disabled","disabled")
     const message=e.target.elements.message.value;
     console.log(message);
     socket.emit("message",message,(error)=>{
+        $messageFormButton.removeAttribute("disabled")
+        $messageFormInput.value=""
+        $messageFormInput.focus()
         if (error) {
             return console.log(error)
         }
@@ -16,16 +27,18 @@ document.getElementById('send').addEventListener("submit",(e)=>{
    
 })
 
-document.querySelector('#send-location').addEventListener("click",()=>{
+locationButton.addEventListener("click",()=>{
    
     if(!navigator.geolocation){
         return alert("Geolocation is not supported by your browser")
     }
+    locationButton.setAttribute('disabled',"disabled")
 
     navigator.geolocation.getCurrentPosition((position)=>{
         const positionObject={lat:position.coords.latitude,lon:position.coords.longitude}
         socket.emit("sendlocation",positionObject,()=>{
             console.log("location shared");
+            locationButton.removeAttribute('disabled')
         })
         console.log(position.coords.latitude);
     })
